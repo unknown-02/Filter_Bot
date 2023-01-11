3cb7b1b05253a32a010aaa6366a0ba4a49d456ab
Bots.Business <git.sync@bots.business>
2023-01-11 05:57:05 +0000

    Bots.Business export for bot id: 794960

--- bot/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @SLBotsOfficial
-
-from .bot import Bot
-
-app = Bot()
-app.run()
---
+++ commands/_start.js
@@ -0,0 +1,16 @@
+/*CMD
+  command: /start
+  help: help
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+Hello User,
+You just send the movie or series name with CORRECT SPELLING and I will forward the links of the same to you..!!
+🤝🤝
+  ANSWER
+  keyboard: Contacts
+  aliases: hi, hello
+CMD*/
+
